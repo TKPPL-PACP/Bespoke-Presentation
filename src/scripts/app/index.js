fa8637b0139d5aa3f87bcb91164293b5f962dd67
Denny Ho <denny.ho@mikroskil.ac.id>
2016-05-24 15:05:05 +0700

    tambah fitur 10 match history per accound id

--- src/scripts/app/index.js
+++ src/scripts/app/index.js
@@ -8,6 +8,24 @@
       'src.scripts.test',       
 
       'scripts.app.directive.test-http',
-    ]);
+
+      'scripts.app.directive.match-history',
+
+    ])
+    .controller("mainController", mainController);
+
+    mainController.$inject = [
+        "$scope",
+	    "$filter", 
+	    "$http"
+	];
+
+	function mainController($scope, $filter, $http) {
+    	var urlGetHeroes = "https://api.steampowered.com/IEconDOTA2_570/GetHeroes/v0001/?key=C3CB04CAA921917E39D9F8329E4A8130&language=en_us";
+    	$http.get(urlGetHeroes).then( function(response) {
+    		$scope.heroes = response.data.result.heroes;
+    		console.log(response.data.result.heroes);
+    	});
+	}
 
 })();
