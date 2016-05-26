954713341b9a77dc8fca78a6af7f67c460613a39
Denny Ho <denny.ho@mikroskil.ac.id>
2016-05-26 15:18:56 +0700

    tambah ambil nickname dan hero untuk api match detail

--- src/scripts/app/directive/match-detail.js
+++ src/scripts/app/directive/match-detail.js
@@ -24,19 +24,34 @@
     return {
       restrict: 'AE',
       controller:function($scope) {
-        $scope.matchID = 2382708300;
+        $scope.matchID = 2386390039;
         function find() {
           var url = "https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/?key=C3CB04CAA921917E39D9F8329E4A8130&match_id="+$scope.matchID;
-          $http.get(url).then(function (response) {
-            $scope.datas = response.data.result;
-            for(var i=0;i<$scope.datas.players.length;i++) {
-              console.log($scope.datas.players[i].account_id);
-              var accId = $scope.datas.players[i].account_id;
-              console.log(ConvertBit.convertTo64Bit(accId));
-            }
-          }, function() {
-            alert("Turn out I got nothing. Try again");
-          });
+          var urlSteamProfile = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/";
+          var steamids = "";
+
+          urlSteamProfile +=  "?key=C3CB04CAA921917E39D9F8329E4A8130";
+          urlSteamProfile += "&steamids=";
+
+          setTimeout(function() {
+              $http.get(url).then(function (response) {
+                for(var i=0;i<response.data.result.players.length;i++) {
+                  var accId = response.data.result.players[i].account_id;
+                  if(i) steamids += ",";
+                  steamids += ConvertBit.convertTo64Bit(accId);
+                }
+                setTimeout(function() {
+                    $http.get(urlSteamProfile + steamids).then(function (response2) {
+                        $scope.datas = response.data.result;
+                        $scope.profiles = response2.data.response.players;
+                    }, function() {
+                        alert('These accounts has been blocked maybe ...');
+                    });
+                }, 1500);
+              }, function() {
+                alert("Turn out I got nothing. Try again");
+              });
+          }, 1500);
         }
         $scope.find = find;
       }
